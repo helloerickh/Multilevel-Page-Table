dcb4e868a40dd770543ea4f0500e3296ec104f1f
Erick Hernandez <helloerickh@gmail.com>
2022-03-16 15:18:52 -0700

    tracereader main put in main.cpp

--- Makefile
+++ Makefile
@@ -1,11 +1,14 @@
-CXX = g++ 
-CXXFLAGS = -std=c++11 -g
+CXX=g++ 
+CXXFLAGS=-std=c++11 -g
 
-pagingwithtlb: main.o
-	$(CXX) $(CXXFLAGS) -o paginwithtlb $^
+pagingwithtlb: main.o tracereader.o output_mode_helpers.o
+	$(CXX) $(CXXFLAGS) -o pagingwithtlb $^
 
-main.o: main.cpp
+main.o: main.cpp tracereader.h output_mode_helpers.h
+
+tracereader.o: tracereader.h tracereader.cpp
 
 output_mode_helpers.o: output_mode_helpers.h output_mode_helpers.cpp
 
-tracereader.o: tracereader.h tracereader.cpp
+clean: 
+	rm *.o
---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include <stdio.h>  //FILE
+#include <stdlib.h> //exit
+#include "tracereader.h"
+#include "output_mode_helpers.h"
+
+void AddressDecoder(p2AddrTr *addr_ptr, FILE *out);
+
+int main(int argc, char **argv)
+{
+  FILE *ifp;	        /* trace file */
+  unsigned long i = 0;  /* instructions processed */
+  p2AddrTr trace;	/* traced address */
+
+  /* check usage */
+  if(argc != 2) {
+    fprintf(stderr,"usage: %s input_byutr_file\n", argv[0]);
+    exit(1);
+  }
+  
+  /* attempt to open trace file */
+  if ((ifp = fopen(argv[1],"rb")) == NULL) {
+    fprintf(stderr,"cannot open %s for reading\n",argv[1]);
+    exit(1);
+  }
+	
+  while (!feof(ifp)) {
+    /* get next address and process */
+    if (NextAddress(ifp, &trace)) {
+      AddressDecoder(&trace, stdout);
+      i++;
+      if ((i % 100000) == 0)
+	fprintf(stderr,"%dK samples processed\r", i/100000);
+    }
+  }	
+
+  /* clean up and return success */
+  fclose(ifp);
+  return (0);
+}
